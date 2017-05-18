328d1147af4fc641f40ec2767d3964e99d50b01c
mrcportillo <marcossportillo@gmail.com>
2017-05-18 15:00:55 -0300

    commit inicial

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by mrc on 02/05/17.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
